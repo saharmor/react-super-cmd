bb988ef28f587a86a2d130842d58a482c8ffba33
saharhash <saharhashai@gmail.com>
2020-12-17 11:23:08 +0100

    Curated examples page

--- example/src/App.js
+++ example/src/App.js
@@ -9,28 +9,33 @@ import OfflineBoltOutlined from "@material-ui/icons/OfflineBoltOutlined";
 import CommandLineModal from "react-super-cmd";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import {List, ListItem, ListItemText} from "@material-ui/core";
+import {Link, List, ListItem, ListItemText} from "@material-ui/core";
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const useStyles = (theme) => ({
   root: {
     backgroundColor: '#fbfafa',
-    height: '100vh',
+    height: '100%',
     display: 'flex',
     flexDirection: 'column',
     flex: '1',
-    padding: '30px',
+    padding: '50px 30px 10px 30px',
+  },
+  headline: {
+    marginBottom: '10px',
   },
   cmd: {
     backgroundColor: '#dedede',
   },
   listBullet: {
+    fontSize: '10px',
     marginRight: '3px',
   },
 });
 
 const App = ({classes}) => {
-  const [cmdLineModal, setCmdLineModal] = useState(true);
+  const [cmdLineModal, setCmdLineModal] = useState(false);
   const [lastRunCmd, setLastRunCmd] = useState(null);
 
   const commands = {
@@ -70,12 +75,22 @@ const App = ({classes}) => {
 
   return (
     <GlobalHotKeys keyMap={keyMap} handlers={handlers}>
-      <Grid className={classes.root} container alignItems='center' spacing={3}>
+      <Grid className={classes.root} container justify='center' alignItems='center' spacing={3}>
         <Grid item>
-          <Typography align="center" variant="h3">react-super-cmd <span role="img" aria-label="lightning-emoji">⚡</span></Typography>
+          <Typography className={classes.headline} align="center" variant="h4">
+            react-super-cmd <span role="img" aria-label="lightning-emoji">⚡</span>
+          </Typography>
           <Typography align="center" variant="h6">The command line experience for the no-mouse generation.</Typography>
           <Typography align="center" variant="h6">A blazing fast command line to allow your users to seamlessly interact with your React app.</Typography>
         </Grid>
+        {!lastRunCmd && <Grid item>
+          <Typography align="center" variant="body1">
+            Give it a spin <span role="img" aria-label="point-emoji">👉</span> Press <code className={classes.cmd}>cmd+k</code>
+          </Typography>
+        </Grid>}
+        {lastRunCmd && <Grid item>
+          <Typography variant="h4">Command selected <code className={classes.cmd}>{lastRunCmd}</code></Typography>
+        </Grid>}
         <Grid item>
           <img width='500px' src={require('./static/demo_image.png')} alt="Command"/>
         </Grid>
@@ -83,7 +98,7 @@ const App = ({classes}) => {
           <Typography variant="h6">Possible applications</Typography>
           <List dense>
             <ListItem>
-              <FiberManualRecordIcon className={classes.listBullet} fontSize="small"/>
+              <FiberManualRecordIcon className={classes.listBullet}/>
               <ListItemText primary="In-app navigation" primaryTypographyProps={{variant: "body1"}}/>
             </ListItem>
             <ListItem>
@@ -92,20 +107,26 @@ const App = ({classes}) => {
             </ListItem>
             <ListItem>
               <FiberManualRecordIcon className={classes.listBullet} fontSize="small"/>
-              <ListItemText primary="Any other use cases required a list of options to select from" primaryTypographyProps={{variant: "body1"}}/>
+              <ListItemText primary="Any other use cases requiring a list of options to select from" primaryTypographyProps={{variant: "body1"}}/>
             </ListItem>
           </List>
         </Grid>
+        <Grid item>
+          <Grid item container direction="row" spacing={1}>
+            <Grid item>
+              <GitHubIcon/>
+            </Grid>
+            <Grid item>
+              <Typography variant={"body1"}>
+                <Link href="https://github.com/saharmor/react-super-cmd" target="_blank" rel="noreferrer">
+                  By Sahar Mor
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
         <CommandLineModal commands={commands} isOpen={cmdLineModal} toggleIsModalOpen={toggleIsOpen}
                           title={"Super Command"} logo={<OfflineBoltOutlined/>}/>
-        {!lastRunCmd && <Grid item>
-          <Typography align="center" variant="body1">
-            Give it a spin <span role="img" aria-label="point-emoji">👉</span> Press cmd+k to start <span role="img" aria-label="magic-emoji">✨</span>
-          </Typography>
-        </Grid>}
-        {lastRunCmd && <Grid item>
-          <Typography variant="h4">Command selected <code className={classes.cmd}>{lastRunCmd}</code></Typography>
-        </Grid>}
       </Grid>
     </GlobalHotKeys>
   )
