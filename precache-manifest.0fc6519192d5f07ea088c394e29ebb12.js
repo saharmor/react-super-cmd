93c71ccc9a3f155d0772dfad6af3d398046e2856
saharhash <saharhashai@gmail.com>
2020-12-17 13:38:27 +0100

    Updates

---
+++ precache-manifest.0fc6519192d5f07ea088c394e29ebb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc2a729ff7a30031c396659c00e97ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c26fa52502bfe7f516e4",
+    "url": "./static/css/main.793b73a5.chunk.css"
+  },
+  {
+    "revision": "cdc91f52e762d41f3e32",
+    "url": "./static/js/2.9873c52b.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.9873c52b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26fa52502bfe7f516e4",
+    "url": "./static/js/main.6ee12268.chunk.js"
+  },
+  {
+    "revision": "1728cfffd524ab87c754",
+    "url": "./static/js/runtime-main.b2988166.js"
+  },
+  {
+    "revision": "be4e42ce9463f84f88e008d21d26f7a5",
+    "url": "./static/media/demo_image.be4e42ce.png"
+  }
+]);
